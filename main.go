1598435351e7f34d57f0c55e31172f52d86a70f1
Nicolas Scordamaglia <nicolas.scordamaglia@lifullconnect.com>
2025-08-14 12:16:39 -0300

    Refactor: move analyze() from main.go to analyze.go

--- main.go
+++ main.go
@@ -273,44 +273,7 @@ func formatSignalMessage(symbol, action, price string) string {
 	return msg
 }
 
-func analyze(symbol string, ts *techan.TimeSeries) string {
-	closePrices := techan.NewClosePriceIndicator(ts)
-	emaShort := techan.NewEMAIndicator(closePrices, 9)
-	emaLong := techan.NewEMAIndicator(closePrices, 21)
-
-	rsi := techan.NewRelativeStrengthIndexIndicator(closePrices, 14)
-
-	macd := techan.NewMACDIndicator(closePrices, 12, 26)
-	macdSignal := techan.NewMACDHistogramIndicator(macd, 9)
-
-	lastIdx := ts.LastIndex()
-	if lastIdx < 26 {
-		return "WAIT"
-	}
-
-	emaShortNow := emaShort.Calculate(lastIdx)
-	emaLongNow := emaLong.Calculate(lastIdx)
-	emaShortPrev := emaShort.Calculate(lastIdx - 1)
-	emaLongPrev := emaLong.Calculate(lastIdx - 1)
-
-	rsiVal := rsi.Calculate(lastIdx)
-	macdVal := macd.Calculate(lastIdx)
-	macdSignalVal := macdSignal.Calculate(lastIdx)
-
-	if emaShortNow.GT(emaLongNow) && emaShortPrev.LTE(emaLongPrev) &&
-		rsiVal.LT(big.NewDecimal(70)) &&
-		macdVal.GT(macdSignalVal) {
-		return "BUY"
-	}
-
-	if emaShortNow.LT(emaLongNow) && emaShortPrev.GTE(emaLongPrev) &&
-		rsiVal.GT(big.NewDecimal(30)) &&
-		macdVal.LT(macdSignalVal) {
-		return "SELL"
-	}
-
-	return "HOLD"
-}
+// analyze moved to analyze.go
 
 func main() {
 	// Check for backtest flag first before parsing
